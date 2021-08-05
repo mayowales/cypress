90c70d06dddade7bb8f50fe2a8a9319d17d1a722
Wale <walekolade45@gmail.com>
2021-08-05 21:46:38 +0200

    First cypress test

---
+++ cypress/integration/myFirstTest.js
@@ -0,0 +1,34 @@
+
+describe('my first cypress test', () => {
+    it('should show error message with wrong password', () => {
+        //user should be able to navigate to the website
+      cy.visit('/') 
+     // user should be able t
+      cy.get('.nav__button-secondary').click()
+
+      it*
+
+      cy.url().should('include', 'login')
+
+      cy.get('#username').type("walekolade45@gmail.com")
+      cy.get('#password').type('wrongpassword')
+      cy.get('.btn__primary--large').click()
+
+    })
+
+    it('successfully load and login', () => {
+        cy.visit('/')
+        cy.get('.nav__button-secondary').click()
+        cy.url().should('include', 'login')
+
+        cy.get('#username').type("walekolade45@gmail.com")
+        cy.get('#password').type("Mayo#213")
+        cy.get('.btn__primary--large').click()
+
+
+    })
+
+  })
+
+
+  
